0a43aeac58239e69bacfe0813bfb568b67026688
wangeric3 <wangeric3@gmail.com>
2020-12-10 22:17:24 +0800

    clean up code

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
-# DL_Final_Project
+# CSCI 1470/2470 Final Project
+## Contributors
+Jorge Isaac Chang Ortega: jchang88
+
+Eric Wang: ewang34 	
+
+Ruotao Zhang: rzhang63
+
+## File Summary
+count_packages.py: Main script for running the package counting. Loads in model trained by train_fasterRCNN.py. Implements package counting with object tracking by utilizing sort.py.
+
+sort.py: SORT object tracking API implemented by Bewley et al. Uses filterpy for Kalman Filter implementation. 
+
+@inproceedings{Bewley2016_sort,
+  author={Bewley, Alex and Ge, Zongyuan and Ott, Lionel and Ramos, Fabio and Upcroft, Ben},
+  booktitle={2016 IEEE International Conference on Image Processing (ICIP)},
+  title={Simple online and realtime tracking},
+  year={2016},
+  pages={3464-3468},
+  keywords={Benchmark testing;Complexity theory;Detectors;Kalman filters;Target tracking;Visualization;Computer Vision;Data Association;Detection;Multiple Object Tracking},
+  doi={10.1109/ICIP.2016.7533003}
+}
+
+train_fasterRCNN.py: trains the Faster RCNN network
+
+video2frames.py: helper script for splitting videos into frames for labeling.
+
+
--- count_packages.py
+++ count_packages.py
@@ -1,4 +1,3 @@
-#CoM line paremter
 import numpy as np
 import argparse
 import imutils
@@ -18,27 +17,22 @@ from sort import *
 
 ap = argparse.ArgumentParser()
 ap.add_argument("-i", "--input", required=True,
-	help="path to input video")
+	help="input video path or all for all videos")
 ap.add_argument("-b", "--batch", type=int, default=4,
 	help="batch size")
+ap.add_argument("-c", "--confidence", type=float, default=0.5,
+	help="min confidence for detection")
 ap.add_argument("-l", "--line", type=int, default=1,
 	help="virtual line: 0 for top diagonal, 1 for middle horizontal, 2 for bottom diagonal")
 ap.add_argument("-a", "--age", type=int, default=1,
 	help="max age for keeping trackers")
-ap.add_argument("-c", "--confidence", type=float, default=0.5,
-	help="minimum probability to filter weak detections")
-ap.add_argument("-t", "--threshold", type=float, default=0.3,
-	help="threshold when applyong non-maxima suppression")
+
 args = vars(ap.parse_args())
 
 lines = [[(0, 0), (160, 120)],
         [(0, 120), (160, 120)],
         [(0, 120), (160, 230)]]
 line = lines[args["line"]]
-
-np.random.seed(42)
-COLORS = np.random.randint(0, 255, size=(200, 3),
-	dtype="uint8")
 
 if not os.path.exists('sort_output'):
     os.makedirs('sort_output')
@@ -47,12 +41,11 @@ try:
 except NameError:
     current_file_dir = os.getcwd().replace('\\', '/')
 
-# Return true if line segments AB and CD intersect
 def intersect(A,B,C,D):
-	return ccw(A,C,D) != ccw(B,C,D) and ccw(A,B,C) != ccw(A,B,D)
+	return (int_helper(A,C,D) != int_helper(B,C,D)) and (int_helper(A,B,C) != int_helper(A,B,D))
 
-def ccw(A,B,C):
-	return (C[1]-A[1]) * (B[0]-A[0]) > (B[1]-A[1]) * (C[0]-A[0])
+def int_helper(A,B,C):
+	return (C[1] - A[1]) * (B[0] - A[0]) > (B[1] - A[1]) * (C[0] - A[0])
 
 def get_model_instance(num_classes):
     model_urls['fasterrcnn_resnet50_fpn_coco'] = model_urls['fasterrcnn_resnet50_fpn_coco'].replace('https://', 'http://')
@@ -100,9 +93,33 @@ def unpack_prediction(pred, threshold):
         pred_score = []
     return pred_boxes, pred_class, pred_score
 
+def update_counter(b, ids, prev_bb, counter, curr_frame):
+    if len(b) <= 0:
+        return counter, curr_frame
+    for i in range(len(b)):
+        width = int(b[i][2])
+        height = int(b[i][3])
+        if ids[i] in prev_bb:
+            prev = prev_bb[ids[i]]
+            x1 = int(b[i][0])
+            y1 = int(b[i][1])
+            x2 = int(prev[0])
+            y2 = int(prev[1])
+            width2 = int(prev[2])
+            height2 = int(prev[3])
+            com_line = [((int(x1 + (width - x1) / 2), int(y1 + (height - y1) / 2))),
+                        ((int(x2 + (width2 - x2) / 2), int(y2 + (height2 - y2) / 2)))]
+            cv2.line(curr_frame, com_line[0], com_line[1], (255, 0, 255), 3)
+            #Check if virtual line and com line intersects
+            #Where count increses
+            if intersect(com_line[0], com_line[1], line[0], line[1]):
+                counter += 1
+        cv2.rectangle(curr_frame, (int(b[i][0]), int(b[i][1])), (width, height), (255, 0, 255), 2)
+    return counter, curr_frame
+
 def do_prediction(model, input_path):
-    tracker = Sort(max_age=args["age"])
     memory = {}
+    tracker = Sort(max_age=args["age"])
     counter = 0
     vs = cv2.VideoCapture(input_path)
     writer = None
@@ -110,23 +127,17 @@ def do_prediction(model, input_path):
 
     frameIndex = 0
 
-    # try to determine the total number of frames in the video file
+    # count total frames
     try:
         prop = cv2.cv.CV_CAP_PROP_FRAME_COUNT if imutils.is_cv2() \
             else cv2.CAP_PROP_FRAME_COUNT
         total = int(vs.get(prop))
         print("[INFO] {} total frames in video ".format(total) + input_path)
-
-    # an error occurred while trying to determine the total
-    # number of frames in the video file
     except:
         print("[INFO] could not determine # of frames in video")
-        print("[INFO] no approx. completion time can be provided")
         total = -1
 
-    # loop over frames from the video file stream
     while True:
-        # if the frame dimensions are empty, grab them
         frame_batch = []
         for i in range(args["batch"]):
             (grabbed, frame) = vs.read()
@@ -142,116 +153,69 @@ def do_prediction(model, input_path):
         #Pass batch of frames through our network. 
         start = time.time()
         pred_boxes, pred_class, pred_score = get_batch_prediction(model, frame_batch, 0.5)
-        # pred_boxes, pred_class, pred_score = ([[],[],[],[]],[[],[],[],[]],[[],[],[],[]])
         end = time.time()
 
         for j in range(len(frame_batch)):
-            curr_frame = frame_batch[j]
-            # initialize our lists of detected bounding boxes, confidences,
-            # and class IDs, respectively
-            boxes = []
             confidences = []
             classIDs = []
-            
+            curr_frame = frame_batch[j]
+            boxes = []
             numBoxes = len(pred_score[j])
-            # loop over each of the detections
             for i in range(numBoxes):
-                # extract the class ID and confidence
-                # of the current object detection
                 con = pred_score[j][i]
                 b = pred_boxes[j][i]
 
-                # filter out weak predictions by ensuring the detected
-                # probability is greater than the minimum probability
                 if con > args["confidence"]:
                     (x0, y0) = b[0] #upper left corner
                     (x1, y1) = b[1] #bottom right corner
-
-                    # update our list of bounding box coordinates,
-                    # confidences, and class IDs
-                    boxes.append([int(x0), int(y0), int(x1-x0), int(y1-y0)])
                     confidences.append(float(con))
+                    #calculate left x, left y, width, height
+                    boxes.append([int(x0), int(y0), int(x1-x0), int(y1-y0)])
+                    # all predictions will be 1
                     classIDs.append(1)
 
-            # apply non-maxima suppression to suppress weak, overlapping
-            # bounding boxes
-            idxs = cv2.dnn.NMSBoxes(boxes, confidences, args["confidence"], args["threshold"])
+            suppression = cv2.dnn.NMSBoxes(boxes, confidences, 0.6, 0.3)#0.4, 0.2
 
             dets = []
-            if len(idxs) > 0:
-                # loop over the indexes we are keeping
-                for i in idxs.flatten():
-                    (x, y) = (boxes[i][0], boxes[i][1])
-                    (w, h) = (boxes[i][2], boxes[i][3])
-                    dets.append([x, y, x+w, y+h, confidences[i]])
-
-            np.set_printoptions(formatter={'float': lambda x: "{0:0.3f}".format(x)})
+            if len(suppression) > 0:
+                suppression = suppression.flatten()
+                for i in suppression:
+                    #Keep box that passes suprresion theshhold
+                    dets.append([boxes[i][0], boxes[i][1], boxes[i][0]+boxes[i][2], boxes[i][1]+boxes[i][3], confidences[i]])
             dets = np.asarray(dets)
             tracks = tracker.update(dets)
 
-            boxes = []
-            indexIDs = []
-            c = []
-            previous = memory.copy()
+            b = []
+            ids = []
+            prev_bb = memory.copy()
             memory = {}
 
-            for track in tracks:
-                boxes.append([track[0], track[1], track[2], track[3]])
-                indexIDs.append(int(track[4]))
-                memory[indexIDs[-1]] = boxes[-1]
+            for k in range(len(tracks)):
+                ids.append(int(tracks[k][4]))
+                extracted_box = [tracks[k][0], tracks[k][1], tracks[k][2], tracks[k][3]]
+                b.append(extracted_box)
+                memory[ids[-1]] = b[-1]
 
-            if len(boxes) > 0:
-                i = int(0)
-                for box in boxes:
-                    # extract the bounding box coordinates
-                    (x, y) = (int(box[0]), int(box[1]))
-                    (w, h) = (int(box[2]), int(box[3]))
+            #Check if packages cross virtual line and update counter
+            counter, curr_frame = update_counter(b, ids, prev_bb, counter, curr_frame)
+            #write line to frame
+            cv2.line(curr_frame, line[0], line[1], (255, 0, 255), 5)
+            #write counter to frame
+            cv2.putText(curr_frame, str(counter), (350,250), cv2.FONT_HERSHEY_SIMPLEX, 1.0, (255, 0, 255), 5)
 
-                    # draw a bounding box rectangle and label on the image
-                    color = [int(c) for c in COLORS[indexIDs[i] % len(COLORS)]]
-                    cv2.rectangle(curr_frame, (x, y), (w, h), color, 2)
-
-                    if indexIDs[i] in previous:
-                        previous_box = previous[indexIDs[i]]
-                        (x2, y2) = (int(previous_box[0]), int(previous_box[1]))
-                        (w2, h2) = (int(previous_box[2]), int(previous_box[3]))
-                        p0 = (int(x + (w-x)/2), int(y + (h-y)/2))
-                        p1 = (int(x2 + (w2-x2)/2), int(y2 + (h2-y2)/2))
-                        cv2.line(curr_frame, p0, p1, color, 3)
-
-                        if intersect(p0, p1, line[0], line[1]):
-                            counter += 1
-
-                    # text = "{}: {:.4f}".format(LABELS[classIDs[i]], confidences[i])
-                    text = "{}".format(indexIDs[i])
-                    cv2.putText(curr_frame, text, (x, y - 5), cv2.FONT_HERSHEY_SIMPLEX, 0.5, color, 2)
-                    i += 1
-
-            # draw line
-            cv2.line(curr_frame, line[0], line[1], (0, 255, 255), 5)
-            # draw counter
-            cv2.putText(curr_frame, str(counter), (350,250), cv2.FONT_HERSHEY_DUPLEX, 1.0, (0, 255, 255), 5)
-            # saves image file
-            # cv2.imwrite("sort_output/frame-{}.png".format(frameIndex), curr_frame)
-
-            # check if the video writer is None
-            if writer is None:
+            if writer == None:
                 fps = vs.get(cv2.CAP_PROP_FPS)
                 fourcc = cv2.VideoWriter_fourcc(*"MJPG")
                 output_name = "out_" + input_path.split("/")[-1]
                 writer = cv2.VideoWriter("sort_output/" + output_name, fourcc, fps,
                     (curr_frame.shape[1], curr_frame.shape[0]), True)
-                if total > 0:
-                    elap = (end - start)
-                    print("[INFO] single batch took {:.4f} seconds".format(elap))
-                    print("[INFO] estimated total time to finish: {:.4f}".format(
-                        elap * total / args["batch"]))
+                print("single batch: ", (end-start), " total estimate: ", (end - start)*total / args["batch"])
 
             writer.write(curr_frame)
             frameIndex += 1
 
             # Uncomment this to return at specific frame
-            #if frameIndex >= 100:
+            # if frameIndex >= 240:
             #    writer.release()
             #    vs.release()
             #    print("Final count for ", input_path, ": ", counter)
